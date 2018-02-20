2c8bcdc6e7b0d9f92db883b4a4b9a586067c2583
rbruinekool <rbruinekool@gmail.com>
2018-02-20 16:47:57 +0100

    The mutebox.py script is now run from the server instead of a crontab and can be restarted from the webpanel

--- talkbackWebapp.py
+++ talkbackWebapp.py
@@ -72,4 +72,4 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
     start_runner()
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
